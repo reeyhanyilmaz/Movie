d88639dcc0595aee847b685ab1d54e605a5432c6
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2023-11-24 16:50:09 +0300

    side bar styles and  add icons

--- src/components/Movies/SideBar.js
+++ src/components/Movies/SideBar.js
@@ -1,22 +1,37 @@
 import React from "react";
 import Toggle from "@/components/Movies/Toggle";
+import Image from "next/image";
 
 function SideBar() {
   return (
     <div className="w-1/6 flex flex-col items-start pt-11 pl-8 min-h-screen bg-[#F9F9F9] border-r-2 border-[ligthModeBorderColor]">
-      <p className="text-base">MENU</p>
-      <button className="mt-6 text-base">Discovery</button>
-      <button className="mt-8 text-base">Top Rated</button>
-      <button className="mt-8 text-base">Coming Soon</button>
-      <p className="mt-10">LIBRARY</p>
-      <button className="mt-6 text-base">Recent Played</button>
-      <button className="mt-8 text-base">Download</button>
-      <div className="flex mt-8">
-        <span className="text-base">Dark Mode</span>
+      <p className="text-base text-lightGrayTextColor">MENU</p>
+      <button className="mt-6 text-base font-semibold text-grayTextColor flex">
+        <Image className="mr-3" src="/movies/discover.svg" width={24} height={24} /> Discovery
+      </button>
+      <button className="mt-8 text-base font-semibold text-blackTextColor flex">
+        <Image className="mr-3" src="/movies/pink_star.svg" width={24} height={24} /> Top Rated
+      </button>
+      <button className="mt-8 text-base font-semibold text-grayTextColor flex">
+        <Image className="mr-3" src="/movies/coming_soon.svg" width={24} height={24} /> Coming Soon
+      </button>
+      <p className="mt-10 text-lightGrayTextColor">LIBRARY</p>
+      <button className="mt-6 text-base font-semibold text-grayTextColor flex">
+        <Image className="mr-3" src="/movies/recent_player.svg" width={24} height={24} /> Recent Played
+      </button>
+      <button className="mt-8 text-base font-semibold  text-grayTextColor flex">
+        <Image className="mr-3" src="/movies/download.svg" width={24} height={24} /> Download
+      </button>
+      <div className="mt-8 flex items-center justify-center">
+        <span className="text-base font-semibold text-grayTextColor flex">
+          <Image className="mr-3" src="/movies/moon.svg" width={24} height={24} /> Dark Mode 
+        </span>
         <Toggle />
       </div>
 
-      <button className="mt-8 text-base">Settings</button>
+      <button className="mt-8 text-base font-semibold  text-grayTextColor flex">
+        <Image className="mr-3" src="/movies/setting.svg" width={24} height={24} /> Settings
+      </button>
     </div>
   );
 }
