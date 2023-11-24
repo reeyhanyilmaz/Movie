df3e96075edc63eb60aed5c1fe8f2c26686cd6f6
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2023-11-24 20:37:58 +0300

    add domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["*", "tailwindui.com", "images.unsplash.com", "m.media-amazon.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
