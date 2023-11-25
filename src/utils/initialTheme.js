0ef42142b96a23e0bba1f39795eddfa503785dc6
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2023-11-25 16:23:06 +0300

    delete theme utils file

--- src/utils/initialTheme.js
+++
@@ -1,8 +0,0 @@
-import { setInitialTheme } from "./theme";
-import React, { useEffect } from "react";
-
-export const initialTheme = () => {
-  useEffect(() => {
-    setInitialTheme(); // Tema kayıtlımı kontrol eder ve ayarlar
-  }, []);
-};
