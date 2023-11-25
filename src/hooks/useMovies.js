86755f07de07cf3725dc40c0eef7f553c2e7d906
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2023-11-25 22:11:26 +0300

    api fetch from rapidapi

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,38 +1,39 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 import moviesData from "../data/movies.json";
+
 export default function useMovies() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [movies, setMovies] = useState(false);
 
   useEffect(() => {
-    // const fetchData = async () => {
-    //   setLoading(true);
-    //   await axios
-    //     .get("https://imdb-top-100-movies.p.rapidapi.com/", {
-    //       headers: {
-    //         "X-RapidAPI-Key": "52468f2f54msh985f69293a2eb70p120022jsnac3aaff0b50f",
-    //         "X-RapidAPI-Host": "imdb-top-100-movies.p.rapidapi.com",
-    //       },
-    //     })
-    //     .then((response) => {
-    //       setMovies(response.data);
-    //       setLoading(false);
-    //       console.log("top of rated", response.data);
-    //     })
-    //     .catch((error) => {
-    //       setError(true);
-    //       setLoading(false);
-    //     });
-    // };
+    const fetchData = async () => {
+      setLoading(true);
+      await axios
+        .get("https://imdb-top-100-movies.p.rapidapi.com/", {
+          headers: {
+            "X-RapidAPI-Key": "62df342172msh6f6ae24a5da15afp12fd93jsn6610f9009b43",
+            "X-RapidAPI-Host": "imdb-top-100-movies.p.rapidapi.com",
+          },
+        })
+        .then((response) => {
+          setMovies(response.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setError(true);
+          setLoading(false);
+        });
+    };
 
-    // fetchData();
+    fetchData();
 
-    setLoading(true);
-    setTimeout(() => {
-      setMovies(moviesData);
-      setLoading(false);
-    }, 1000);
+    // setLoading(true);
+    // setTimeout(() => {
+    //   setMovies(moviesData);
+    //   setLoading(false);
+    // }, 1000);
   }, []);
 
   return { error, loading, movies };
--- src/hooks/useSeries.js
+++ src/hooks/useSeries.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 import seriesData from "../data/series.json";
 
 export default function useSeries() {
@@ -7,32 +8,31 @@ export default function useSeries() {
   const [series, setSeries] = useState(false);
 
   useEffect(() => {
-    // const fetchData = async () => {
-    //   setLoading(true);
-    //   await axios
-    //     .get("https://imdb-top-100-movies.p.rapidapi.com/series", {
-    //       headers: {
-    //         "X-RapidAPI-Key": "52468f2f54msh985f69293a2eb70p120022jsnac3aaff0b50f",
-    //         "X-RapidAPI-Host": "imdb-top-100-movies.p.rapidapi.com",
-    //       },
-    //     })
-    //     .then((response) => {
-    //       setSeries(response.data);
-    //       setLoading(false);
-    //       console.log("top of rated", response.data);
-    //     })
-    //     .catch((error) => {
-    //       setError(true);
-    //       setLoading(false);
-    //     });
-    // };
-    // fetchData();
+    const fetchData = async () => {
+      setLoading(true);
+      await axios
+        .get("https://imdb-top-100-movies.p.rapidapi.com/series", {
+          headers: {
+            "X-RapidAPI-Key": "62df342172msh6f6ae24a5da15afp12fd93jsn6610f9009b43",
+            "X-RapidAPI-Host": "imdb-top-100-movies.p.rapidapi.com",
+          },
+        })
+        .then((response) => {
+          setSeries(response.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setError(true);
+          setLoading(false);
+        });
+    };
+    fetchData();
 
-    setLoading(true);
-    setTimeout(() => {
-      setSeries(seriesData);
-      setLoading(false);
-    }, 1000);
+    // setLoading(true);
+    // setTimeout(() => {
+    //   setSeries(seriesData);
+    //   setLoading(false);
+    // }, 1000);
   }, []);
 
   return { error, loading, series };
